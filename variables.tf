4b480421265e81748619eda4dec479747da57ec4
Dincho Todorov <dincho@users.noreply.github.com>
2019-08-13 16:22:24 +0300

    Bump lb module version (#7)
    * Bump lb module version
      - enable sticky sessions for external & internal APIs
      - add env tag to some resources for tracking/information purposes
      - enable all debug endpoints of internal API
    * Bump bootstrap version
    * Bump default package version

--- variables.tf
+++ variables.tf
@@ -3,11 +3,11 @@ variable "vault_addr" {
 }
 
 variable "bootstrap_version" {
-  default = "v2.6.2"
+  default = "v2.6.5"
 }
 
 variable "package" {
-  default = "https://releases.ops.aeternity.com/aeternity-4.0.0-ubuntu-x86_64.tar.gz"
+  default = "https://releases.ops.aeternity.com/aeternity-4.1.0-ubuntu-x86_64.tar.gz"
 }
 
 variable "dns_zone" {
