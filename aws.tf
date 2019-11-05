e1d02054601bacf6eaee66604a7542acbd24f2af
Ivaylo Badinov <632282+venimus@users.noreply.github.com>
2019-11-05 11:30:12 +0200

    Bump to 5.0.2 (#17)

--- aws.tf
+++ aws.tf
@@ -9,18 +9,18 @@ terraform {
 
 # Default
 provider "aws" {
-  version = "2.16.0"
+  version = "2.33.0"
   region  = "us-east-1"
 }
 
 provider "aws" {
-  version = "2.16.0"
+  version = "2.33.0"
   region  = "us-east-1"
   alias   = "us-east-1"
 }
 
 provider "aws" {
-  version = "2.16.0"
+  version = "2.33.0"
   region  = "eu-north-1"
   alias   = "eu-north-1"
 }
--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 module "nodes_api_uat_stockholm" {
-  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.0"
+  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.3.1"
   env               = "api_uat"
   envid             = "api_uat"
-  bootstrap_version = "${var.bootstrap_version}"
+  bootstrap_version = var.bootstrap_version
   vault_role        = "ae-node"
-  vault_addr        = "${var.vault_addr}"
+  vault_addr        = var.vault_addr
 
   static_nodes   = 0
   spot_nodes_min = 2
@@ -18,10 +18,10 @@ module "nodes_api_uat_stockholm" {
   additional_storage_size = 30
   snapshot_filename       = "mnesia_uat_v-1_latest.tgz"
 
-  asg_target_groups = "${module.lb_uat_stockholm.target_groups}"
+  asg_target_groups = module.lb_uat_stockholm.target_groups
 
   aeternity = {
-    package = "${var.package}"
+    package = var.package
   }
 
   providers = {
@@ -30,13 +30,13 @@ module "nodes_api_uat_stockholm" {
 }
 
 module "lb_uat_stockholm" {
-  source                    = "github.com/aeternity/terraform-aws-api-loadbalancer?ref=v1.1.0"
+  source                    = "github.com/aeternity/terraform-aws-api-loadbalancer?ref=v1.2.0"
   env                       = "api_uat"
-  fqdn                      = "${var.lb_fqdn}"
-  dns_zone                  = "${var.dns_zone}"
-  security_group            = "${module.nodes_api_uat_stockholm.sg_id}"
-  vpc_id                    = "${module.nodes_api_uat_stockholm.vpc_id}"
-  subnets                   = "${module.nodes_api_uat_stockholm.subnets}"
+  fqdn                      = var.lb_fqdn
+  dns_zone                  = var.dns_zone
+  security_group            = module.nodes_api_uat_stockholm.sg_id
+  vpc_id                    = module.nodes_api_uat_stockholm.vpc_id
+  subnets                   = module.nodes_api_uat_stockholm.subnets
   internal_api_enabled      = true
   state_channel_api_enabled = true
 
@@ -48,9 +48,9 @@ module "lb_uat_stockholm" {
 module "gateway_uat" {
   source          = "github.com/aeternity/terraform-aws-api-gateway?ref=v3.0.1"
   env             = "api_uat"
-  dns_zone        = "${var.dns_zone}"
-  api_domain      = "${var.domain}"
-  api_aliases     = "${var.domain_aliases}"
-  certificate_arn = "${aws_acm_certificate_validation.cert.certificate_arn}"
-  lb_fqdn         = "${var.lb_fqdn}"
+  dns_zone        = var.dns_zone
+  api_domain      = var.domain
+  api_aliases     = var.domain_aliases
+  certificate_arn = aws_acm_certificate_validation.cert.certificate_arn
+  lb_fqdn         = var.lb_fqdn
 }
