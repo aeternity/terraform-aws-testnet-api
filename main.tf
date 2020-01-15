8db6b92442ee99113869bac35a778629787e015b
Ivaylo Papazov <papazov@vmandco.com>
2020-01-15 17:12:17 +0200

    Bump gateway to 3.1.2

--- main.tf
+++ main.tf
@@ -93,7 +93,7 @@ module "lb_uat_singapore" {
 }
 
 module "gateway_uat" {
-  source          = "github.com/aeternity/terraform-aws-api-gateway?ref=v3.1.1"
+  source          = "github.com/aeternity/terraform-aws-api-gateway?ref=v3.1.2"
   env             = "api_uat"
   dns_zone        = var.dns_zone
   api_domain      = var.domain
