00ad05c50ceab4c4117f7827b0db04339ee212c1
Ivaylo Papazov <IvayloPapazov@users.noreply.github.com>
2020-04-22 19:21:28 +0300

    Add stand-alone FSM node to handle WS requests (#38)

--- main.tf
+++ main.tf
@@ -30,10 +30,11 @@ module "nodes_api_uat_stockholm" {
 }
 
 module "lb_uat_stockholm" {
-  source                    = "github.com/aeternity/terraform-aws-api-loadbalancer?ref=v1.2.0"
+  source                    = "github.com/aeternity/terraform-aws-api-loadbalancer?ref=v1.3.0"
   env                       = "api_uat"
   fqdn                      = var.lb_fqdn
   dns_zone                  = var.dns_zone
+  sc_security_group         = module.nodes_api_uat_stockholm_channels.sg_id
   security_group            = module.nodes_api_uat_stockholm.sg_id
   vpc_id                    = module.nodes_api_uat_stockholm.vpc_id
   subnets                   = module.nodes_api_uat_stockholm.subnets
@@ -73,6 +74,40 @@ module "nodes_api_uat_singapore" {
 
   providers = {
     aws = "aws.ap-southeast-1"
+  }
+}
+
+module "nodes_api_uat_stockholm_channels" {
+  source            = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v2.5.0"
+  env               = "api_uat"
+  envid             = "api_uat"
+  bootstrap_version = var.bootstrap_version
+  vault_role        = "ae-node"
+  vault_addr        = var.vault_addr
+  subnets           = module.nodes_api_uat_stockholm.subnets
+  vpc_id            = module.nodes_api_uat_stockholm.vpc_id
+
+  enable_state_channels = true
+
+  static_nodes   = 1
+  spot_nodes_min = 0
+  spot_nodes_max = 0
+
+  spot_price    = "0.15"
+  instance_type = "c5.xlarge"
+  ami_name      = "aeternity-ubuntu-16.04-v1549009934"
+
+  additional_storage      = true
+  additional_storage_size = 60
+
+  asg_target_groups = module.lb_uat_stockholm.target_groups_channels
+
+  aeternity = {
+    package = var.package
+  }
+
+  providers = {
+    aws = "aws.eu-north-1"
   }
 }
 
