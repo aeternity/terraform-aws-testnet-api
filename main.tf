88181f2609195f40cca44adf3c698518b4a05e2d
Dincho Todorov <dincho@users.noreply.github.com>
2025-04-09 12:53:06 +0300

    Remove Singapore and SC node

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "nodes_api_uat_stockholm" {
   env    = "api_uat"
 
   static_nodes   = 0
-  spot_nodes_min = 2
+  spot_nodes_min = 1
   spot_nodes_max = 10
 
   instance_type  = "c6i.xlarge"
@@ -39,110 +39,110 @@ module "lb_uat_stockholm" {
   env                       = "api_uat"
   fqdn                      = var.lb_fqdn
   dns_zone                  = var.dns_zone
-  sc_security_group         = module.nodes_api_uat_stockholm_channels.sg_id
+  # sc_security_group         = module.nodes_api_uat_stockholm_channels.sg_id
   security_group            = module.nodes_api_uat_stockholm.sg_id
   vpc_id                    = module.nodes_api_uat_stockholm.vpc_id
   subnets                   = module.nodes_api_uat_stockholm.subnets
   internal_api_enabled      = true
-  state_channel_api_enabled = true
+  state_channel_api_enabled = false
 
   providers = {
     aws = aws.eu-north-1
   }
 }
 
-module "nodes_api_uat_singapore" {
-  source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
-  env    = "api_uat"
+# module "nodes_api_uat_singapore" {
+#   source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
+#   env    = "api_uat"
 
-  static_nodes   = 0
-  spot_nodes_min = 2
-  spot_nodes_max = 10
+#   static_nodes   = 0
+#   spot_nodes_min = 2
+#   spot_nodes_max = 10
 
-  instance_type  = "c6i.xlarge"
-  instance_types = ["c6i.xlarge", "c5d.xlarge", "c5.xlarge"]
-  ami_name       = "aeternity-ubuntu-22.04-v1720022468"
+#   instance_type  = "c6i.xlarge"
+#   instance_types = ["c6i.xlarge", "c5d.xlarge", "c5.xlarge"]
+#   ami_name       = "aeternity-ubuntu-22.04-v1720022468"
 
-  root_volume_size        = 24
-  additional_storage      = true
-  additional_storage_size = 300
+#   root_volume_size        = 24
+#   additional_storage      = true
+#   additional_storage_size = 300
 
-  asg_target_groups = module.lb_uat_singapore.target_groups
+#   asg_target_groups = module.lb_uat_singapore.target_groups
 
-  tags = {
-    role  = "aenode"
-    env   = "api_uat"
-    envid = "api_uat"
-  }
+#   tags = {
+#     role  = "aenode"
+#     env   = "api_uat"
+#     envid = "api_uat"
+#   }
 
-  config_tags = {
-    vault_role        = "ae-node"
-    vault_addr        = var.vault_addr
-    bootstrap_version = var.bootstrap_version
-    bootstrap_config  = "secret2/aenode/config/api_uat"
-  }
+#   config_tags = {
+#     vault_role        = "ae-node"
+#     vault_addr        = var.vault_addr
+#     bootstrap_version = var.bootstrap_version
+#     bootstrap_config  = "secret2/aenode/config/api_uat"
+#   }
 
-  providers = {
-    aws = aws.ap-southeast-1
-  }
-}
+#   providers = {
+#     aws = aws.ap-southeast-1
+#   }
+# }
 
-module "nodes_api_uat_stockholm_channels" {
-  source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
-  env    = "api_uat"
+# module "nodes_api_uat_stockholm_channels" {
+#   source = "github.com/aeternity/terraform-aws-aenode-deploy?ref=v4.0.0"
+#   env    = "api_uat"
 
-  static_nodes   = 1
-  spot_nodes_min = 0
-  spot_nodes_max = 0
+#   static_nodes   = 1
+#   spot_nodes_min = 0
+#   spot_nodes_max = 0
 
-  instance_type  = "t3.large"
-  instance_types = ["t3.large", "c5.large", "m5.large"]
-  ami_name       = "aeternity-ubuntu-22.04-v1709639419"
+#   instance_type  = "t3.large"
+#   instance_types = ["t3.large", "c5.large", "m5.large"]
+#   ami_name       = "aeternity-ubuntu-22.04-v1709639419"
 
-  root_volume_size        = 20
-  additional_storage      = true
-  additional_storage_size = 200
+#   root_volume_size        = 20
+#   additional_storage      = true
+#   additional_storage_size = 200
 
-  asg_target_groups = module.lb_uat_stockholm.target_groups_channels
-  subnets           = module.nodes_api_uat_stockholm.subnets
-  vpc_id            = module.nodes_api_uat_stockholm.vpc_id
+#   asg_target_groups = module.lb_uat_stockholm.target_groups_channels
+#   subnets           = module.nodes_api_uat_stockholm.subnets
+#   vpc_id            = module.nodes_api_uat_stockholm.vpc_id
 
-  enable_state_channels = true
+#   enable_state_channels = true
 
-  tags = {
-    role  = "aenode"
-    kind  = "channel"
-    env   = "api_uat"
-    envid = "api_uat"
-  }
+#   tags = {
+#     role  = "aenode"
+#     kind  = "channel"
+#     env   = "api_uat"
+#     envid = "api_uat"
+#   }
 
-  config_tags = {
-    vault_role        = "ae-node"
-    vault_addr        = var.vault_addr
-    bootstrap_version = var.bootstrap_version
-    bootstrap_config  = "secret2/aenode/config/api_uat_channel"
-  }
+#   config_tags = {
+#     vault_role        = "ae-node"
+#     vault_addr        = var.vault_addr
+#     bootstrap_version = var.bootstrap_version
+#     bootstrap_config  = "secret2/aenode/config/api_uat_channel"
+#   }
 
-  providers = {
-    aws = aws.eu-north-1
-  }
-}
+#   providers = {
+#     aws = aws.eu-north-1
+#   }
+# }
 
-module "lb_uat_singapore" {
-  source                    = "github.com/aeternity/terraform-aws-api-loadbalancer?ref=v2.0.0"
-  env                       = "api_uat"
-  fqdn                      = var.lb_fqdn
-  dns_zone                  = var.dns_zone
-  security_group            = module.nodes_api_uat_singapore.sg_id
-  vpc_id                    = module.nodes_api_uat_singapore.vpc_id
-  subnets                   = module.nodes_api_uat_singapore.subnets
-  internal_api_enabled      = true
-  state_channel_api_enabled = true
+# module "lb_uat_singapore" {
+#   source                    = "github.com/aeternity/terraform-aws-api-loadbalancer?ref=v2.0.0"
+#   env                       = "api_uat"
+#   fqdn                      = var.lb_fqdn
+#   dns_zone                  = var.dns_zone
+#   security_group            = module.nodes_api_uat_singapore.sg_id
+#   vpc_id                    = module.nodes_api_uat_singapore.vpc_id
+#   subnets                   = module.nodes_api_uat_singapore.subnets
+#   internal_api_enabled      = true
+#   state_channel_api_enabled = true
 
-  providers = {
-    aws = aws.ap-southeast-1
-  }
-}
+#   providers = {
+#     aws = aws.ap-southeast-1
+#   }
+# }
 
 module "gateway_uat" {
   source          = "github.com/aeternity/terraform-aws-api-gateway?ref=v4.0.0"
